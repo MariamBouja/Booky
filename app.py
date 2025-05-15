ef4c6d65fb3cf4785505da99a799aea36c49902a
Wahiba04 <habhobaa123@gmail.com>
2025-05-15 17:06:49 +0100

    RECEIVED the link to access app web

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("hello")
+from flask import Flask
+from psycopg2 import sql
+import config
+
+app = Flask(__name__)
+app.secret_key = config.SECRET_KEY
+ 
+# Database connection function
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=config.DB_NAME,
+        user=config.DB_USER,
+        password=config.DB_PASS,
+        host=config.DB_HOST,
+        port=config.DB_PORT
+    )
+    conn.autocommit = True
+    return conn
+if __name__ == "__main__":
+    app.run(debug=True)
--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # Database configuration
 DB_NAME = "project" 
 DB_USER = "postgres" 
-DB_PASS = "postgres" 
+DB_PASS = "W230604" 
 DB_HOST = "localhost" 
 DB_PORT = "5432"
 
 # Flask configuration
 SECRET_KEY = "dev"
+#added
