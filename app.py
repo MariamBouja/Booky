6feb2409feeb024c0bb1f1dbdc32002472a15b73
Wahiba04 <habhobaa123@gmail.com>
2025-05-15 17:28:46 +0100

    run python

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
- HEAD print("hi")
+from flask import Flask
+from psycopg2 import sql
+import config
 
-print("hello") 
- 022ab94cb58dbc546cd895257dbb555a3bc5cefa
+app = Flask(__name__)
+app.secret_key = config.SECRET_KEY
+ 
+# Database connection function
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=config.DB_NAME,
+        user=config.DB_USER,
+        password=config.DB_PASS,
+        host=config.DB_HOST,
+        port=config.DB_PORT
+    )
+    conn.autocommit = True
+    return conn
+if __name__ == "__main__":
+    app.run(debug=True)
